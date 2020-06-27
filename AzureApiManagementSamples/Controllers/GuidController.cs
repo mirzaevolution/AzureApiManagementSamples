22ec966f54f9ef26093590bb2ba7e6d60c134619
Mirza Ghulam Rasyid <mgr@voxteneo.asia>
2020-06-27 20:14:50 +0700

    adding guid controller

---
+++ AzureApiManagementSamples/Controllers/GuidController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureApiManagementSamples.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuidController : ControllerBase
+    {
+        [HttpGet("")]
+        [ProducesResponseType(200)]
+        public IActionResult Get() => Ok(new
+        {
+            data = new string[]
+            {
+                Guid.NewGuid().ToString(),
+                Guid.NewGuid().ToString(),
+                Guid.NewGuid().ToString(),
+                Guid.NewGuid().ToString(),
+                Guid.NewGuid().ToString()
+            }
+        });
+    }
+}
